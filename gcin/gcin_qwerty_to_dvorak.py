41d158447b7048320568ad68b649c330664fcc1e
Shaform <shaform@gmail.com>
2019-01-18 18:49:10 -0500

    update to python3

--- gcin/gcin_qwerty_to_dvorak.py
+++ gcin/gcin_qwerty_to_dvorak.py
@@ -1,42 +1,47 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # gcin_qwerty_to_dvorak.py
 """
 Usage: python gcin_qwerty_to_dvorak.py input.cin > output.cin
 """
 import fileinput
 import re
-from string import maketrans
 
 QWERTY = '''-=qwertyuiop[]sdfghjkl;'zxcvbn,./_+QWERTYUIOP{}SDFGHJKL:"ZXCVBN<>?'''
 DVORAK = '''[]',.pyfgcrl/=oeuidhtns-;qjkxbwvz{}"<>PYFGCRL?+OEUIDHTNS_:QJKXBWVZ'''
-TRANS = maketrans(QWERTY, DVORAK)
+TRANS = str.maketrans(QWERTY, DVORAK)
 
-re_option = re.compile(r'%(selkey|endkey)\s+(\S+)')
-re_begin = re.compile(r'%\S+\s+begin')
-re_keydef = re.compile(r'^([^#]\S*)(\s+\S+)')
-re_end = re.compile(r'%\S+\s+end')
 
-begin = False
-for line in fileinput.input():
-    # process key definitions
-    if begin:
-        if re_end.match(line):
-            begin = False
-        else:
-            new_def = re_keydef.subn(lambda x: x.group(1).translate(TRANS) +
-                    x.group(2), line)
-            # print transformed line
-            if new_def[1] != 0:
-                print new_def[0],
-                continue
-        # print comment/end lines
-        print line,
-        continue
+def main():
+    re_option = re.compile(r'%(selkey|endkey)\s+(\S+)')
+    re_begin = re.compile(r'%\S+\s+begin')
+    re_keydef = re.compile(r'^([^#]\S*)(\s+\S+)')
+    re_end = re.compile(r'%\S+\s+end')
 
-    m = re_option.match(line)
-    if m is not None:
-        print '%%%s %s' % (m.group(1), m.group(2).translate(TRANS))
-        continue
-    elif re_begin.match(line):
-        begin = True
-    print line,
+    begin = False
+    for line in fileinput.input():
+        # process key definitions
+        if begin:
+            if re_end.match(line):
+                begin = False
+            else:
+                new_def = re_keydef.subn(
+                    lambda x: x.group(1).translate(TRANS) + x.group(2), line)
+                # print transformed line
+                if new_def[1] != 0:
+                    print(new_def[0], end='')
+                    continue
+            # print comment/end lines
+            print(line, end='')
+            continue
+
+        m = re_option.match(line)
+        if m is not None:
+            print('%%%s %s' % (m.group(1), m.group(2).translate(TRANS)))
+            continue
+        elif re_begin.match(line):
+            begin = True
+        print(line, end='')
+
+
+if __name__ == '__main__':
+    main()
--- xrdp/xrdpkeymap_qwerty_to_dvorak.py
+++ xrdp/xrdpkeymap_qwerty_to_dvorak.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # xrdpkeymap_qwerty_to_dvorak.py
 """
 Usage: python xrdpkeymap_qwerty_to_dvorak.py km-input.ini > km-output.ini
@@ -9,49 +9,45 @@ import re
 # keycodes
 # $ xmodmap -pk for current mapping
 # ref: http://forums.fedoraforum.org/showthread.php?t=265100
-QWERTY = (
-        20, 21,
-        24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35,
-        38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48,
-        52, 53, 54, 55, 56, 57, 58, 59, 60, 61
-        )
+QWERTY = (20, 21, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 38, 39, 40,
+          41, 42, 43, 44, 45, 46, 47, 48, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+          61)
 
-DVORAK = (
-        34, 35,
-        48, 59, 60, 33, 29, 41, 42, 54, 27, 46, 61, 21,
-        38, 32, 26, 30, 31, 40, 43, 28, 57, 39, 20,
-        47, 24, 44, 45, 53, 56, 58, 25, 55, 52
-        )
+DVORAK = (34, 35, 48, 59, 60, 33, 29, 41, 42, 54, 27, 46, 61, 21, 38, 32, 26,
+          30, 31, 40, 43, 28, 57, 39, 20, 47, 24, 44, 45, 53, 56, 58, 25, 55,
+          52)
 
-TRANS_DICT = dict(zip(QWERTY, DVORAK))
+TRANS_DICT = dict(list(zip(QWERTY, DVORAK)))
 
 
-re_title = re.compile('\[.*\]\n')
-re_keydef = re.compile('Key(\d+)=(.*)\n')
-buf = []
-buf_dict = {}
-
-def output_keydefs():
-    global buf, buf_dict
-    for k in buf:
-        if k in TRANS_DICT:
-            print 'Key%d=%s' % (k, buf_dict[TRANS_DICT[k]])
-        else:
-            print 'Key%d=%s' % (k, buf_dict[k])
+def main():
+    re_title = re.compile(r'\[.*\]\n')
+    re_keydef = re.compile(r'Key(\d+)=(.*)\n')
     buf = []
     buf_dict = {}
 
+    def output_keydefs():
+        for k in buf:
+            if k in TRANS_DICT:
+                print('Key%d=%s' % (k, buf_dict[TRANS_DICT[k]]))
+            else:
+                print('Key%d=%s' % (k, buf_dict[k]))
+        return [], {}
 
-for line in fileinput.input():
+    for line in fileinput.input():
         if re_title.match(line) is not None:
-            output_keydefs()
-            print line,
+            buf, buf_dict = output_keydefs()
+            print(line, end='')
         elif re_keydef.match(line) is not None:
             m = re_keydef.match(line)
             buf.append(int(m.group(1)))
             buf_dict[int(m.group(1))] = m.group(2)
         else:
-            print line,
+            print(line, end='')
 
-# output remaining keys
-output_keydefs()
+    # output remaining keys
+    output_keydefs()
+
+
+if __name__ == '__main__':
+    main()
